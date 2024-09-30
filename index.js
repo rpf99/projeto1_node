156cd13e81843f8aec9fb57c4f94438983b78e70
ALUNO C24 <alunoc24@acad.unifor.br>
2024-09-30 20:16:40 -0300

    alterei a rota de produtos

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", function(req,res){
 
 //rota do cadastro de produtos
 app.get("/produtos", function(req,res){
-    res.send("<h1>Lista de Produtos!</h1>");
+    res.send("<h1>Lista de Produtos! versão 4 </h1>");
 })
 
 //rota com parametro 
